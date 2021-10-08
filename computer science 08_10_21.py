966f6b91f64fafc5a8c28355ebc8a785785b50a8
18BShittah <92168343+18BShittah@users.noreply.github.com>
2021-10-08 15:11:10 +0100

    Add files via upload

---
+++ computer science 08_10_21.py
@@ -0,0 +1,34 @@
+name=input("Please write your first and second name")
+print("Welcome" + name)
+
+ 
+
+age=float(input("What is your age?: "))
+print("you are" , age)
+
+ 
+number1 = float (input("pick a number"))
+number2 = float (input("pick another number"))
+print(number1 + number2)
+
+number3 = float (input("pick a number"))
+number4 = float (input("pick another number"))
+print(number3 - number4)
+
+number5 = float (input("pick a number"))
+number6 = float (input("pick another number"))
+print(number5 * number6)
+
+number7 = float (input("pick a number"))
+number8 = float (input("pick another number"))
+print13(number7 / number8)
+
+number9 = float (input("pick a number"))
+number10 = float (input("pick another number"))
+print(number9 // number10)
+
+number11 = float (input("pick a number"))
+number12 = float (input("pick another number"))
+print(number11 % number12)
+
+print( (number1 + number2 + number3)/3 )
